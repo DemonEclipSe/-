e2386fa4cd5650423e54b08c5274ba22ceec7a34
DemonEclipSe <48463140+DemonEclipSe@users.noreply.github.com>
2020-09-03 06:06:48 +0100

    Update load.lua

--- load.lua
+++ load.lua
@@ -3,19 +3,19 @@
 
 if _G.ScriptNum == 1 then
     -- Adding them later.
-    return "To be added!"
+    return print("To be added!")
 elseif _G.ScriptNum == 2 then
     -- Adding them later.
-    return "To be added!"
+    return print("To be added!")
 elseif _G.ScriptNum == 3 then
     -- Adding them later.
-    return "To be added!"
+    return print("To be added!")
 elseif _G.ScriptNum == 4 then
     -- Adding them later.
-    return "To be added!"
+    return print("To be added!")
 elseif _G.ScriptNum == 5 then
     -- Adding them later.
-    return "To be added!"
+    return print("To be added!")
 else
-    return "Invalid number. Choose from 1 to 5."
+    return print("Invalid number. Choose from 1 to 5.")
 end
