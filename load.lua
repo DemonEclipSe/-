113c4dc407376c40c8ed52fc2d1cb2848d64cfc3
DemonEclipSe <48463140+DemonEclipSe@users.noreply.github.com>
2020-09-03 05:54:44 +0100

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,20 @@
+-- Thanks for using my script today.
+
+
+if _G.ScriptNum == 1 then
+    -- Adding them later.
+    return "To be added!"
+elseif _G.ScriptNum == 2 then
+    -- Adding them later.
+    return "To be added!"
+elseif _G.ScriptNum == 3 then
+    -- Adding them later.
+    return "To be added!"
+elseif _G.ScriptNum == 4 then
+    -- Adding them later.
+    return "To be added!"
+elseif _G.ScriptNum == 5 then
+    -- Adding them later.
+    return "To be added!"
+else
+    return "Invalid number. Choose from 1 to 5."
